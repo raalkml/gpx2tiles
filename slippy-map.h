c9ae7ff205960479d331e7c25335a6513b4e83e2
Alex Riesen <raa.lkml@gmail.com>
2016-10-25 21:00:00 +0200

    Cleanup slippy-map.h for readability

--- slippy-map.h
+++ slippy-map.h
@@ -9,20 +9,23 @@
 
 static inline int long2tilex(double lon, int z)
 {
-	return (int)(floor((lon + 180.0) / 360.0 * pow(2.0, z)));
+	double x = (lon + 180.0) / 360.0 * pow(2.0, z);
+	return (int)floor(x);
 }
 
 static inline int lat2tiley(double lat, int z)
 {
-	return (int)(floor((1.0 - log( tan(lat * M_PI/180.0) + 1.0 / cos(lat * M_PI/180.0)) / M_PI) / 2.0 * pow(2.0, z))); 
+	double lrad = lat * M_PI / 180.0;
+	double y = (1.0 - log(tan(lrad) + 1.0 / cos(lrad)) / M_PI) / 2.0 * pow(2.0, z);
+	return (int)floor(y);
 }
 
-static inline double tilex2long(int x, int z) 
+static inline double tilex2long(int x, int z)
 {
 	return x / pow(2.0, z) * 360.0 - 180;
 }
 
-static inline double tiley2lat(int y, int z) 
+static inline double tiley2lat(int y, int z)
 {
 	double n = M_PI - 2.0 * M_PI * y / pow(2.0, z);
 	return 180.0 / M_PI * atan(0.5 * (exp(n) - exp(-n)));
